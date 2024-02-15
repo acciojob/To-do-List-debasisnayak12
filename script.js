3cfae995f5f4b0f1314f39e848284faff5d9ccd5
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2024-02-15 09:15:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+let container = document.getElementById('todoList');
+let addBtn = document.getElementById('addTodoBtn');
 
+addBtn.addEventListener('click', (e)=>{
+	e.preventDefault();
+	let inputValue = document.getElementById('newTodoInput').value;
+	if(inputValue !== ""){
+		let li = document.createElement('li');
+		li.textContent = inputValue;
+		container.appendChild(li);
+	}
+	document.getElementById('newTodoInput').value = '';
+	
+});
